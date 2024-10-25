74ccd97654a75acbf89fb2d252364e9db21efb94
Shanmukha1947 <142641933+Shanmukha1947@users.noreply.github.com>
2024-10-25 20:49:32 +0530

    Add files via upload

---
+++ Task-191978-M-B-T2/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
---
+++ Task-191978-M-B-T2/src/Main1.java
@@ -0,0 +1,11 @@
+public class Main1 {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        System.out.println("Original string: " + str);
+        String uppercaseStr = str.toUpperCase();
+        System.out.println("Uppercase string: " + uppercaseStr);
+        String lowercaseStr = str.toLowerCase();
+        System.out.println("Lowercase string: " + lowercaseStr);
+    }
+}
+
